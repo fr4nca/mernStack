857f3e017d42fe007e3e3fb05093b0e17bf69533
Victor França <fr4ncacc@gmail.com>
2018-05-11 01:45:46 -0300

    Post register works

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,41 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/User')
+const gravatar = require('gravatar')
+const bcrypt = require('bcryptjs')
 
 // @route   GET api/users/teste
 // @dsc     tests users route
 // @access  Public
-router.get('/teste', (req, res) => res.json({msg: "Users works"}))
+router.get('/teste', (req, res) => res.json({ msg: "Users works" }))
+
+// @route   POST api/users/register
+// @dsc     register user
+// @access  Public
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email }).then((user) => {
+      if (user) {
+        return res.status(400).json({ email: "Email exists" })
+      } else {
+        let avatar = gravatar.url(req.body.email, { s: 200, r: 'pg', d: 'mm' })
+        let newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          avatar,
+          password: req.body.password
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err
+            newUser.password = hash
+            newUser.save()
+              .then((user) => res.json(user))
+              .catch((err) => console.log(err))
+          })
+        })
+      }
+    })
+})
 
 module.exports = router
--- server.js
+++ server.js
@@ -3,14 +3,15 @@ const mongoose = require('mongoose')
 const users = require('./routes/api/users')
 const profile = require('./routes/api/profile')
 const posts = require('./routes/api/posts')
+const bodyParser = require('body-parser')
 
-//Configura servidor
+//Express
 const app = express()
-app.use('/api/users', users)
-app.use('/api/profile', profile)
-app.use('/api/posts', posts)
-const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`Server running on port ${port}`))
+
+//Middlewares
+//Body parser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 //Configura mongoose
 const db = require('./config/keys').mongoURI
@@ -19,8 +20,15 @@ mongoose
   .then(() => console.log("Connected to mongo"))
   .catch((err) => console.log(err))
 
+//Configura rotas
+app.use('/api/users', users)
+app.use('/api/profile', profile)
+app.use('/api/posts', posts)
 
 //Rotas
 app.get('/', (req, res) => {
   res.send('Alecrim')
 })
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`Server running on port ${port}`))
